89cfe1efaa9c6bbec87c61cb168f634a692e6003
Leonardo Escossia <leoctescossia11@gmail.com>
2023-01-06 14:22:50 -0300

    sim

---
+++ Segundo Site - Copia/js1.js
@@ -0,0 +1,6 @@
+function clicou(){
+    window.location = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+}
+function clicou2(){
+    window.location = 'https://www.youtube.com/watch?v=PDJLvF1dUek'
+}
